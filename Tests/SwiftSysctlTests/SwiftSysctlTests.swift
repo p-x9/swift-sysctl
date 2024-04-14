1ba2b0cf9f809be8648df0752ed78399df54039f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-15 00:03:28 +0900

    add test case

--- Tests/SwiftSysctlTests/SwiftSysctlTests.swift
+++ Tests/SwiftSysctlTests/SwiftSysctlTests.swift
@@ -1,4 +1,19 @@
 import XCTest
 @testable import SwiftSysctl
 
-final class SwiftSysctlTests: XCTestCase {}
+final class SwiftSysctlTests: XCTestCase {
+    func testSysctlNext() {
+        let root: [Int32] = [0, 0]
+        var current: [Int32]? = root
+
+        while current != nil {
+            guard let _current = current else { break }
+            let name = Sysctl._name(_current)
+            print(
+                name ?? "Unknown",
+                _current
+            )
+            current = Sysctl._next(_current)
+        }
+    }
+}
